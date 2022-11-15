90ed93f06d9c9929f25b7ed6b01e59403921c5bd
Adam Do <23448052+98adamdo@users.noreply.github.com>
2022-11-15 15:16:40 -0800

    Update and rename MissingNumber.java to 0268. Missing Number.java

--- 0268. Missing Number.java
+++ 0268. Missing Number.java
@@ -1,4 +1,4 @@
-class MissingNumber {
+class Solution {
     public int missingNumber(int[] nums) {
         int sum = (nums.length*(nums.length+1))/2;
         for(int i = 0; i < nums.length; i++) {
