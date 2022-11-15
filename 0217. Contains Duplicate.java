bd650100603abcb9db2902305092d7b76dae4092
Adam Do <23448052+98adamdo@users.noreply.github.com>
2022-11-15 15:15:31 -0800

    Update and rename ContainsDuplicate.java to 0217. Contains Duplicate.java

--- 0217. Contains Duplicate.java
+++ 0217. Contains Duplicate.java
@@ -1,5 +1,5 @@
 import java.util.HashSet;
-class ContainsDuplicate {
+class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> hs = new HashSet<>();
         for(int i = 0; i < nums.length; i++) {
